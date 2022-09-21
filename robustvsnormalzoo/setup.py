30d0c7f2edd952e13c4de29a6979059307e6ce96
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-09-21 17:01:10 +0000

    robustvsnormalzoo: adding setup.py

---
+++ robustvsnormalzoo/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="robustvsnormalzoo",
+    version="1.0.0",
+    author="Paul Gavrikov",
+    author_email="paul.gavrikov@hs-offenburg.de",
+    description="A PyTorch model zoo consisting of robust (adversarially trained) image classifiers and otherwise equivalent normal models for research purposes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GeJulia/robust_confidences_evaluation",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "torch",
+        "pandas"
+        "robustbench @ git+https://github.com/RobustBench/robustbench.git@5e3b8bd56a9b6ee52554a63e8a9ea255289a8a12",
+        "geotorch==0.3.0",
+        "torchdiffeq==0.2.3",
+        "timm==0.5.4",
+    ],
+    python_requires=">=3.6",
+)
